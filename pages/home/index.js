420dd646796a86616cc19ae6bef57219dcc16ced
Lucca Haddad <hslucca@gmail.com>
2022-09-22 11:36:38 -0300

    Construção Homepage-1

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,2 +1,16 @@
-/* Desenvolva a lógica da página aqui */
+/* <div id="modalTeste" class="modal__wrapper hide">
+<div class="modal">
+    <div class="modal__div-1">
+        <img class="modal__div1-img" src="src/assets/Vector.svg" alt="">
+        <div class="modal__div1-information">
+            <h3>Nome</h3>
+            <p>Cargo</p>
+        </div>
+    </div>
+    <div class="modal__div-2">
+        <h2>Titulo do post</h2>
+        <p>Post completo</p>      
+    </div>
+</div>
+</div> */
 
